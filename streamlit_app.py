6a6f6fad9472c59be52488dbb3d89d6a4b819ba4
Vladyslav Y <107836940+Minimaruva@users.noreply.github.com>
2024-11-13 23:08:35 +0000

    basic q and a pipeline use

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,19 +1,40 @@
 import streamlit as st
-from transformers import AutoModelForSequenceClassification, AutoTokenizer
+from transformers import pipeline, AutoTokenizer, AutoModelForQuestionAnswering
 import torch
 from io import StringIO
 
 # Show title and description.
 st.title("ChatCV")
 st.write("A simple way to interact with CV")
-
-cv = st.file_uploader(label="Upload your CV in txt format", type="txt")
-
-if cv is not None:
+# File uploader
+uploaded_file = st.file_uploader(label="Upload your uploaded_file in txt format", type="txt")
+# Get data from file if it's uploaded
+if uploaded_file is not None:
     # To convert to a string based IO:
-    stringio = StringIO(cv.getvalue().decode("utf-8"))
+    stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+    # Read file as string:
+    uploaded_file_string_data = stringio.read()
+    st.write(uploaded_file_string_data)
 
-    # To read file as string:
-    cv_string_data = stringio.read()
-    st.write(cv_string_data)
+# Ask a question
+question = st.text_input(
+    "Ask about CV",
+    placeholder="Give me short summary about candidate",
+    disabled=not uploaded_file,
+)
 
+if uploaded_file and question:
+
+    question_answerer = pipeline("question-answering")
+    generator = pipeline("text-generation", model="gpt2")
+    # Split CV into sequences
+    # context = uploaded_file_string_data.split(sep=".")
+    # promt = "You are CV analyser, you give detailed, correct answers only based on provided context"
+
+    context = uploaded_file_string_data
+
+    basic_answer = question_answerer(question=question, context=context)["answer"]
+
+    expanded_answer = generator(f"Question: {question} Answer: {basic_answer}. Could you elaborate?", max_length=50)[0]["generated_text"]
+    st.write(f"Basic answer: {basic_answer}")
+    st.write(f"Expanded answer: {expanded_answer}")
